52debeb379c18f3db977c16ebc495df73022d18a
Felipe <fquiceno1@misena.edu.co>
2022-07-11 12:53:03 -0500

    Ciclo de vida

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 import { OutletContext } from '@angular/router';
 
 @Component({
@@ -6,17 +6,50 @@ import { OutletContext } from '@angular/router';
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit,OnChanges,AfterViewInit,OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
 
   imageDefault = 'https://www.w3schools.com/howto/img_avatar2.png';
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
+    //before render
+    //No async
+    console.log('contructor', 'imgValue =>',this.img);
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      //before - during Render
+      // changes inputs --run many times
+      console.log('ngOnChanges', 'imgValue =>',this.img);
+
+  }
+  ngOnInit(): void {
+    //before render
+    // async -fetch  -- run once time
+    // No changes inputs here.
+    console.log('ngOnInit', 'imgValue =>',this.img);
+
+  }
+  ////
+  ngAfterViewInit(): void {
+    //only after render
+    //handler childs
+    console.log('ngAfterViewInit');
+    throw new Error('Method not implemented.');
+  }
+
+  /////
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    throw new Error('Method not implemented.');
+  }
+
+
+
+
   imgError(){
     this.img = this.imageDefault;
   }
